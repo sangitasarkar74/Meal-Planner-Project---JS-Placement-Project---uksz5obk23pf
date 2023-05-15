110733bf5f8728139214e4b7397654dba8f129e9
sangitasarkar74 <sangitasarkar@gmail.com>
2023-05-15 10:24:34 +0530

    added scroll

--- App.js
+++ App.js
@@ -193,3 +193,12 @@ async function ingredients(id) {
   // console.log(res);
   return data.analyzedInstructions;
 }
+
+const scrollToTop = document.querySelector(".material-symbols-outlined");
+scrollToTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
